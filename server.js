b8094a9bc173c05660ea9c3e02a109de9ba1ab01
Taurus <coolpersoninportugal@gmail.com>
2026-05-06 18:53:43 +0100

    Initial TimeKeeper app with Express server for Railway

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = process.env.PORT || 3000;
+
+app.use(express.static(path.join(__dirname)));
+app.get('*', (_, res) => res.sendFile(path.join(__dirname, 'index.html')));
+app.listen(port, () => console.log(`TimeKeeper running on port ${port}`));
